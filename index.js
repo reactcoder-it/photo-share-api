647e63590a7257acdb5054aa8c5ac6fffad8aa45
Vadim Pashaev <vmp@live.ru>
2020-01-12 19:00:53 +0400

    fix

--- index.js
+++ index.js
@@ -1,29 +1,64 @@
-const { ApolloServer } = require('apollo-server-express')
+const { ApolloServer, PubSub } = require('apollo-server-express')
 const express = require('express')
 const expressPlayground = require('graphql-playground-middleware-express').default
 const { readFileSync } = require('fs')
-const typeDefs = readFileSync('./typeDefs.graphql', 'UTF-8')
 const resolvers = require('./resolvers')
 const { MongoClient } = require('mongodb')
+const { createServer } = require('http')
+const path = require('path')
+const depthLimit = require('graphql-depth-limit')
+const { createComplexityLimitRule } = require('graphql-validation-complexity')
+
 require('dotenv').config()
+const typeDefs = readFileSync('./typeDefs.graphql', 'UTF-8')
 
 async function start() {
   const app = express()
-  const MONGO_DB = process.env.DB_HOST
+  let db
 
-  const client = await MongoClient.connect(MONGO_DB, { useNewUrlParser: true })
-  const db = client.db()
+  try {
+    const client = await MongoClient.connect(process.env.DB_HOST, { useNewUrlParser: true, useUnifiedTopology: true })
+    db = client.db()
+  } catch (error) {
+    console.log(`
+      MongoDB Host not found!
+      Please add DB_HOST environment variable to .env file
 
-  const context = { db }
+      exiting...
+    `)
+    process.exit(1)
+  }
 
-  const server = new ApolloServer({ typeDefs, resolvers, context })
+  const pubsub = new PubSub()
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    engine: true,
+    validationRules: [
+      depthLimit(5),
+      createComplexityLimitRule(1000, {
+        onCost: cost => console.log('query cost: ', cost)
+      })
+    ],
+    context: async ({ req, connection }) => {
+      const githubToken = req ? req.headers.authorization : connection.context.Authorization
+      const currentUser = await db.collection('users').findOne({ githubToken })
+      return { db, currentUser, pubsub }
+    }
+  })
 
   server.applyMiddleware({ app })
 
-  app.get('/', (req, res) => res.end('Welcome to the PhotoShare API!'))
   app.get('/playground', expressPlayground({ endpoint: "/graphql" }))
+  app.get('/', (req, res) => res.end('Welcome to the PhotoShare API!'))
 
-  app.listen({ port: 4000 },
+  app.use('/img/photos', express.static(path.join(__dirname, 'assets', 'photos')))
+
+  const httpServer = createServer(app)
+  server.installSubscriptionHandlers(httpServer)
+  httpServer.timeout = 5000
+
+  httpServer.listen({ port: 4000 },
     () => console.log(`GraphQL Service Running http://localhost:4000${server.graphqlPath}`)
   )
 }
--- resolvers.js
+++ resolvers.js
@@ -1,85 +1,151 @@
 const { GraphQLScalarType } = require("graphql")
-
-var _id = 0
-
-var users = [
-  { "githubLogin": "mHattrup", "name": "Mike Hattrup" },
-  { "githubLogin": "gPlake", "name": "Glen Plake" },
-  { "githubLogin": "sSchmidt", "name": "Scot Schmidt" }
-]
-
-var photos = [{
-  "id": "1",
-  "name": "Dropping the Heart Chute",
-  "description": "The heart chute is one of my favorite chutes",
-  "category": "ACTION",
-  "githubUser": "gPlake",
-  "created": "3-28-1977"
-},{
-  "id": "2",
-  "name": "Enjoying the sunshine",
-  "category": "SELFIE",
-  "githubUser": "sSchmidt",
-  "created": "1-2-1985"
-},{
-  "id": "3",
-  "name": "Gunbarrel 25",
-  "description": "25 laps on gunbarrel today",
-  "category": "LANDSCAPE",
-  "githubUser": "sSchmidt",
-  "created": "2018-04-15T19:09:57.308Z"
-}]
-
-var tags = [
-  { "photoID": "1", "userID": "gPlake" },
-  { "photoID": "2", "userID": "sSchmidt" },
-  { "photoID": "2", "userID": "mHattrup" },
-  { "photoID": "2", "userID": "gPlake" },
-]
+const { authorizeWithGithub } = require('./lib')
+const fetch = require('node-fetch')
+const { ObjectID } = require('mongodb')
+const { uploadStream } = require('./lib')
+const path = require('path')
 
 const resolvers = {
   Query: {
+    me: (parent, args, { currentUser }) => currentUser,
+
     totalPhotos: (parent, args, { db }) => db.collection('photos').estimatedDocumentCount(),
     allPhotos: (parent, args, { db }) => db.collection('photos').find().toArray(),
+    photo: (parent, args, { db }) => db.collection('photos').findOne({ _id: ObjectID(args.id) }),
 
     totalUsers: (parent, args, { db }) => db.collection('users').estimatedDocumentCount(),
-    allUsers: (parent, args, { db }) => db.collection('users').find().toArray()
+    allUsers: (parent, args, { db }) => db.collection('users').find().toArray(),
+    user: (parent, args, { db }) => db.collection('users').findOne({ githubLogin: args.login })
   },
 
   Mutation: {
-    postPhoto: (parent, args) => {
-      var newPhoto = {
-        id: _id++,
+    postPhoto: async (parent, args, { db, currentUser, pubsub }) => {
+      if (!currentUser) {
+        throw new Error("only an authorized user can post a photo")
+      }
+
+      const newPhoto = {
         ...args.input,
+        userID: currentUser.githubLogin,
         created: new Date()
       }
-      photos.push(newPhoto)
+
+      const { insertedId } = await db.collection('photos').insertOne(newPhoto)
+      newPhoto.id = insertedId
+
+      let toPath = path.join(__dirname, 'assets', 'photos', `${newPhoto.id}.jpg`)
+
+      const { createReadStream } = await args.input.file
+      const stream = createReadStream()
+      await uploadStream(stream, toPath)
+
+      pubsub.publish('photo-added', { newPhoto })
+
       return newPhoto
+    },
+
+    tagPhoto: async (parent, args, { db }) => {
+      await db.collection('tags')
+        .replaceOne(args, args, { upsert: true })
+      
+      return db.collection('photos')
+        .findOne({ _id: ObjectID(args.photoID) })
+    },
+
+    githubAuth: async (parent, { code }, { db, pubsub }) => {
+      let { message, access_token, avatar_url, login, name } = await authorizeWithGithub({
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+        code
+      })
+    
+      if (message) {
+        throw new Error(message)
+      }
+    
+      let latestUserInfo = { name, githubLogin: login, githubToken: access_token, avatar: avatar_url }
+      const { ops: [user], result } = await db.collection('users')
+        .replaceOne({ githubLogin: login }, latestUserInfo, { upsert: true })
+      
+      result.upserted && pubsub.publish('user-added', { newUser: user })
+      
+      return { user, token: access_token }
+    },
+
+    addFakeUsers: async (parent, { count }, { db, pubsub }) => {
+      var randomUserAPI = `https://randomuser.me/api/?results=${count}`
+      var { results } = await fetch(randomUserAPI)
+        .then(res => res.json())
+
+      var users = results.map(r => ({
+        githubLogin: r.login.username,
+        name: `${r.name.first} ${r.name.last}`,
+        avatar: r.picture.thumbnail,
+        githubToken: r.login.sha1
+      }))
+
+      await db.collection('users').insert(users)
+
+      var newUsers = await db.collection('users')
+        .find()
+        .sort({ _id: -1 })
+        .limit(count)
+        .toArray()
+      
+      newUsers.forEach(newUser => pubsub.publish('user-added', { newUser }))
+
+      return users
+    },
+
+    fakeUserAuth: async (parent, { githubLogin }, { db }) => {
+      var user = await db.collection('users').findOne({ githubLogin })
+
+      if (!user) {
+        throw new Error(`Cannot find user with githubLogin "${githubLogin}"`)
+      }
+
+      return { token: user.githubToken, user }
+    }
+  },
+
+  Subscription: {
+    newPhoto: {
+      subscribe: (parent, args, { pubsub }) => pubsub.asyncIterator('photo-added')
+    },
+
+    newUser: {
+      subscribe: (parent, args, { pubsub }) => pubsub.asyncIterator('user-added')
     }
   },
 
   Photo: {
-    url: parent => `http://yoursite.com/img/${parent.id}.jpg`,
+    id: parent => parent.id || parent._id,
+    url: parent => `/img/photos/${parent._id}.jpg`,
+    postedBy: (parent, args, { db }) => db.collection('users').findOne({ githubLogin: parent.userID }),
 
-    postedBy: parent => {
-      return users.find(u => u.githubLogin === parent.githubUser)
-    },
+    taggedUsers: async (parent, args, { db }) => {
+      const tags = await db.collection('tags').find().toArray()
 
-    taggedUsers: parent => tags
-      .filter(tag => tag.photoID === parent.id)
-      .map(tag => tag.userID)
-      .map(userID => users.find(u => u.githubLogin === userID))
+      const login = tags
+        .filter(t => t.photoID === parent._id.toString())
+        .map(t => t.githubLogin)
+
+      return db.collection('users').find({ githubLogin: { $in: logins } }).toArray()
+    }
   },
 
   User: {
-    postedPhotos: parent => {
-      return photos.filter(p => p.githubUser === parent.githubLogin)
-    },
+    postedPhotos: (parent, args, { db }) => db.collection("photos").find({ userID: parent.githubLogin }).toArray(),
 
-    inPhotos: parent => tags
-      .filter(tag => tag.userID === parent.id)
-      .map(tag => tag.photoID)
-      .map(photoID => photos.find(p => p.id === photoID))
+    inPhotos: async (parent, args, { db }) => {
+      const tags = await db.collection('tags').find().toArray()
+
+      const photoIDs = tags
+        .filter(t => t.githubLogin === parent.githubLogin)
+        .map(t => ObjectID(t.photoID))
+      
+      return db.collection('photos').find({ _id: { $in: photoIDs } }).toArray()
+    }
   },
 
   DateTime: new GraphQLScalarType({
